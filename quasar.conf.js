4f7953fd8590522c3e333d9a576500aa2996bb55
Carlos Lozano <carlosmanuel.lozano@sngular.com>
2020-05-19 14:40:44 +0200

    Add firebase dependency

--- quasar.conf.js
+++ quasar.conf.js
@@ -13,6 +13,7 @@ module.exports = function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
     boot: [
+      'firebase',
       'router-auth',
       'i18n',
       'axios'
