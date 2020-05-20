14bcb1f9e71cdb94c3927f40d2230c286482094b
Carlos Lozano <carlosmanuel.lozano@sngular.com>
2020-05-20 12:27:49 +0200

    Change on value for modifications impact

--- src/store/store-cards.js
+++ src/store/store-cards.js
@@ -1,94 +1,39 @@
-import Vue from 'vue'
 import { uid } from 'quasar'
 import { firebase } from 'boot/firebase'
 
 const state = {
-  cards: {
-    /*
-    ID1: {
-      url: 'https://placeimg.com/400/300/nature?t=' + Math.random(),
-      text: 'texto_1',
-      checked: false
-    },
-    ID2: {
-      url: 'https://placeimg.com/400/300/nature?t=' + Math.random(),
-      text: 'texto_2',
-      checked: false
-    },
-    ID3: {
-      url: 'https://placeimg.com/400/3000/nature?t=' + Math.random(),
-      text: 'texto_3',
-      checked: false
-    },
-    ID4: {
-      url: 'https://placeimg.com/400/300/nature?t=' + Math.random(),
-      text: 'texto_4',
-      checked: false
-    },
-    ID5: {
-      url: 'https://placeimg.com/400/300/nature?t=' + Math.random(),
-      text: 'texto_5',
-      checked: false
-    },
-    ID6: {
-      url: 'https://placeimg.com/400/300/nature?t=' + Math.random(),
-      text: 'texto_6',
-      checked: false
-    },
-    ID7: {
-      url: 'https://placeimg.com/400/300/nature?t=' + Math.random(),
-      text: 'texto_7',
-      checked: false
-    },
-    ID8: {
-      url: 'https://placeimg.com/400/300/nature?t=' + Math.random(),
-      text: 'texto_8',
-      checked: false
-    },
-    ID9: {
-      url: 'https://placeimg.com/400/300/nature?t=' + Math.random(),
-      text: 'texto_9',
-      checked: false
-    }
-    */
-  }
+  cards: {}
 }
 const mutations = {
-  addCard (state, payload) {
-    Vue.set(state.cards, payload.id, payload.card)
-  },
-  updateCard (state, payload) {
-    Object.assign(state.cards[payload.id], payload.updates)
-  },
-  deleteCard (state, id) {
-    Vue.delete(state.cards, id)
+  setCards (state, value) {
+    state.cards = value
   }
 }
 const actions = {
-  addCard ({ commit, dispatch }, card) {
+  setCards ({ commit }, value) {
+    commit('setCards', value)
+  },
+  addCard ({ dispatch }, card) {
     const id = uid()
     const payload = {
       id: id,
       card: card
     }
-    // commit('addCard', payload)
     dispatch('fbWriteData', payload)
   },
-  updateCard ({ commit }, payload) {
-    commit('updateCard', payload)
+  updateCard ({ dispatch }, payload) {
+    dispatch('fbWriteData', { id: payload.id, card: payload.updates })
   },
-  deleteCard ({ commit }, id) {
-    commit('deleteCard', id)
+  deleteCard ({ dispatch }, id) {
+    dispatch('fbWriteData', { id: id, card: null })
   },
   fbWriteData ({ commit }, payload) {
     firebase.database().ref('cards/' + payload.id).set(payload.card)
   },
-  fbReadData ({ commit }, payload) {
-    // const uid = firebase.auth().currentUser.uid
-    // const result = firebase.database().ref('cards/' + uid)
+  fbReadData ({ commit }) {
     const cards = firebase.database().ref('cards')
-    cards.on('child_added', snapshot => {
-      commit('addCard', { id: snapshot.key, card: snapshot.val() })
+    cards.on('value', snapshot => {
+      commit('setCards', snapshot.val())
     })
   }
 }
