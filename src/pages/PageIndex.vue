e9d94b5d77cf623722877691a3b988ac5d3266df
Carlos Lozano <carlosmanuel.lozano@sngular.com>
2020-05-20 13:56:40 +0200

    Improve Index with background

--- src/pages/PageIndex.vue
+++ src/pages/PageIndex.vue
@@ -1,19 +1,9 @@
 <template>
-  <div class="fixed-center text-center">
-    <p class="text-faded">
-      {{ $t('index.welcome') }}
-    </p>
-    <p>
-      <q-img
-        src="~assets/index.png"
-        style="width:50vw"
-      >
-        <div class="absolute-bottom">
-          <q-btn outline icon="touch_app" class="full-width" to="/game">{{$t('index.goToTheGame')}}</q-btn>
-        </div>
-      </q-img>
-    </p>
-  </div>
+  <q-page class="flex flex-center bg-image">
+    <div class="absolute-bottom q-pb-xl q-pl-sm q-pr-sm">
+      <q-btn size="large" color="blue-10" icon="touch_app" class="full-width" to="/game">{{$t('index.goToTheGame')}}</q-btn>
+    </div>
+  </q-page>
 </template>
 
 <script >
@@ -21,3 +11,15 @@ export default {
   name: 'Error404'
 }
 </script>
+<style scope>
+.bg-image {
+    background-image: url('~assets/index.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+.eighty-vertical-hight{
+  width: 100vh !important;
+  height: 100vh !important;
+}
+</style>
